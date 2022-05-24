5e7a2f69933ba29a599d737126542412a3767de1
DanielSLP <64851517+DanielSLP@users.noreply.github.com>
2022-05-24 22:08:15 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,20 @@
-let svetlo = 0
-let svetloLED = 0
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P13, 90)
-    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.servoWritePin(AnalogPin.P16, 145)
+    pins.digitalWritePin(DigitalPin.P2, 1)
 })
 input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P13, -90)
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P16, 45)
+    pins.digitalWritePin(DigitalPin.P2, 0)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    pins.servoWritePin(AnalogPin.P13, 0)
+    pins.servoWritePin(AnalogPin.P16, 0)
 })
+let svetloLED = 0
+let svetlo = 0
+pins.servoWritePin(AnalogPin.P16, 45)
 basic.forever(function () {
-    svetlo = smarthome.ReadLightIntensity(AnalogPin.P2)
-    serial.writeNumber(svetlo)
+    svetlo = smarthome.ReadLightIntensity(AnalogPin.P1)
+    serial.writeValue("svetlo", svetlo)
     svetloLED = input.lightLevel()
-    serial.writeNumber(svetloLED)
+    serial.writeValue("svetloLED", svetloLED)
 })
